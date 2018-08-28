179a29992f9933c8c7bdc61feebbb076b9dce137
wiladog <wiladog@163.com>
2018-08-28 13:29:19 +0800

    init import

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <van-button type="default" @click="showNotify">测试Notify</van-button>
+        <van-notify id="van-notify" />
+    </div>
+</template>
+
+<script>
+
+    import Notify from '../../../static/vant/notify/index';
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            showNotify() {
+                console.info('showNotify');
+                Notify('dddddd');
+                console.info(Notify);
+            },
+        },
+        created() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ static/vant/notify/notify.js
@@ -0,0 +1,29 @@
+const defaultOptions = {
+    selector: '#van-notify',
+    duration: 3000
+};
+
+export default function Notify(options = {}) {
+    const pages = getCurrentPages();
+    console.info(pages);
+    const ctx = pages[pages.length - 1];
+    console.info(ctx);
+    options = Object.assign({}, defaultOptions, parseParam(options));
+    const el = ctx.selectComponent(options.selector);
+    console.info(el);
+    delete options.selector;
+
+    if (el) {
+        // el.setData({
+        //   ...options
+        // });
+        // {...options} 运行结果其实是与 直接的options 相等的没有明白为什么 要写成  {...options}
+        // 修改成
+        el.setData(options);
+        el.show();
+    }
+}
+
+function parseParam(params = '') {
+    return typeof params === 'object' ? params : {text: params};
+}
